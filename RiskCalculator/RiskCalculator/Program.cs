85a596572c607ef71d73c6c84bdaf2e66a8893b2
Andrew Johnson <LimeyJohnson@gmail.com>
2013-04-19 17:40:36 -0700

    Risk Projects

---
+++ RiskCalculator/RiskCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskCalculator
+{
+    class Program
+    {
+        static double RunCount = 1000;
+        static void Main(string[] args)
+        {
+            RunBattle(14, 10);
+            RunBattle(30, 20);
+        }
+        public static void RunBattle(int AArmy, int DArmy, int ADice = 3, int DDice =2)
+        {
+            RiskCalc calc = new RiskCalc();
+            int aWins = 0, dWins = 0;
+            for(int x = 0; x< RunCount; x++)
+            {
+                if (calc.Battle(AArmy, DArmy, ADice, DDice))
+                {
+                    aWins++;
+                }
+                else
+                {
+                    dWins++;
+                }
+            }
+            Console.WriteLine("Battle {0}:{1} vs {2}:{3} result: {4} - {5} ({6})", AArmy, ADice, DArmy, DDice, aWins, dWins,100*(((double) aWins)/RunCount));
+        }
+    }
+}
---
+++ RiskCalculator/RiskCalculator/RiskCalculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class RiskCalc
+{
+    public static Random random;
+    public RiskCalc()
+    {
+
+    }
+
+    public int AArmyRemaining;
+    public int DArmyRemaining;
+
+    public bool Battle(int AArmy, int DArmy, int ADice, int DDice)
+    {
+        AArmy--; //the attacking army must leave one behind
+        while (AArmy > 0 && DArmy > 0)
+        {
+            int ATempDice = AArmy > ADice ? ADice : AArmy;
+            int DTempDice = DArmy > DDice ? DDice : DArmy;
+            List<int> AResults = PlayerDiceRole(ATempDice);
+            List<int> DResults = PlayerDiceRole(DTempDice);
+
+            while (AResults.Count > 0 && DResults.Count > 0)
+            {
+                if (AResults[0] > DResults[0])
+                {
+                    //Attacker has won this die
+                    DArmy--;
+                }
+                else
+                {
+                    //Defender has won this die
+                    AArmy--;
+                }
+                DResults.RemoveAt(0);
+                AResults.RemoveAt(0);
+            }
+        }
+        AArmyRemaining = AArmy;
+        DArmyRemaining = DArmy;
+        return AArmy > DArmy;
+    }
+    public List<int> PlayerDiceRole(int count)
+    {
+        List<int> result = new List<int>();
+        for (int x = 0; x < count; x++)
+        {
+            result.Add(DiceRoll);
+        }
+        result.Sort(delegate(int x, int y)
+        {
+           return y - x;
+        });
+        return result;
+    }
+    public static int DiceRoll
+    {
+        get
+        {
+            return Rand.Next(1, 7);
+        }
+    }
+    public static Random Rand
+    {
+        get
+        {
+            if (random == null) random = new Random();
+            return random;
+        }
+    }
+}
