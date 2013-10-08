dd8c4b0532201f3a9221c90d0c49b993493998b6
LimeyJohnson <LimeyJohnson@gmail.com>
2013-10-08 10:38:47 -0700

    Added Sudoku lib

---
+++ SudokuLib/SodukiLibUnitTest/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SudokuLib;
+
+namespace SodukiLibUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRows()
+        {
+            Board b = new Board();
+            int x = 0;
+            b.EachSquare(s =>
+                {
+                    x += s.Row;
+                });
+            Assert.AreEqual(x, 324);
+        }
+        [TestMethod]
+        public void TestColumns()
+        {
+            Board b = new Board();
+            int x = 0;
+            b.EachSquare(s =>
+            {
+                x += s.Column;
+            });
+            Assert.AreEqual(x, 324);
+        }
+        [TestMethod]
+        public void FireValueChangedEvent()
+        {
+            Square s = new Square(1, 1);
+            Square eventSquare = null;
+            s.ValueSetEvent += delegate(object sender, Square square)
+            {
+                eventSquare = square;
+            };
+            s.Value = 9;
+            Assert.IsNotNull(eventSquare);
+            Assert.AreEqual(s, eventSquare);
+        }
+        [TestMethod]
+        public void TestBoardComplete()
+        {
+            Board b = new Board();
+            b.EachSquare(s => s.Value = 1);
+            Assert.AreEqual(b.Complete, true);
+        }
+        [TestMethod]
+        public void TestSetValueAfterRemovingPossiblities()
+        {
+            Square s = new Square(1, 1);
+            Square eventSquare = null;
+            s.ValueSetEvent += delegate(object sender, Square square)
+            {
+                eventSquare = square;
+            };
+
+            for (int x = 0; x < 8; x++) s.RemovePossiblity(x);
+            Assert.IsNotNull(eventSquare);
+            Assert.AreEqual(eventSquare.Value, 9);
+        }
+        [TestMethod]
+        public void TestEachSquare()
+        {
+            Board b = new Board();
+            b.EachGroup(4, 5, s => s.RemovePossiblity(5));
+
+            int[] possiblexy = { 3, 4, 5 };
+
+            b.EachSquare(s =>
+                {
+                    if (Array.IndexOf(possiblexy, s.Row) >= 0 && Array.IndexOf(possiblexy, s.Column) >= 0)
+                    {
+                        Assert.AreEqual(s.possibiltiesLeft.Length, 8);
+                    }
+                    else
+                    {
+                        Assert.AreEqual(s.possibiltiesLeft.Length, 9);
+                    }
+                });
+            
+        }
+
+    }
+}
+
---
+++ SudokuLib/SudokuLib/Square.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuLib
+{
+    public class Square
+    {
+        private bool[] m_possiblities = new bool[9];
+        private int? m_value;
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public event EventHandler<Square> ValueSetEvent;
+        public Square(int column, int row)
+        {
+            this.Row = row;
+            this.Column = column;
+            for (int x = 0; x < 9; x++) m_possiblities[x] = true;
+        }
+
+        public int? Value
+        {
+            set
+            {
+
+                this.m_value = value;
+                if (ValueSetEvent != null)
+                {
+                    ValueSetEvent(this, this);
+                }
+
+            }
+            get
+            {
+                return m_value;
+            }
+        }
+        public void RemovePossiblity(int num)
+        {
+            m_possiblities[num] = false;
+            if (possibiltiesLeft.Length == 1)
+            {
+                this.Value = Array.FindIndex(m_possiblities, b => b == true) + 1;
+            }
+
+        }
+        public bool[] possibiltiesLeft
+        {
+            get
+            {
+                return Array.FindAll(m_possiblities, b =>  b );
+            }
+        }
+        
+
+    }
+}
